a721e905a60ec05ac7580785584d6425325fb768
ei17ringo <ei17ringo@gmail.com>
2016-02-03 19:44:20 +0800

    commit init

--- sampleSegue/ViewController.swift
+++ sampleSegue/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // カウント用の変数
+    var myCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +22,36 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func returnMenu(segue:UIStoryboardSegue){
+        //移動画面からの戻り口
+    
+        //myCount変数に、前の画面のtmpCountの値を受け取って
+        let newVC = segue.sourceViewController as! secondViewController
+        
+        myCount = newVC.tmpCount
+        
+        //カウントアップ
+        myCount++
+        
+        //値を出力
+        print("前の画面から戻ってきた時＜\(myCount)＞")
+    }
+    
+    // 画面が切り替わる時
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        
+        //カウントアップ
+        myCount++;
+        
+        // 次の画面のtmpCountに、myCountの値を受け渡す
+        var newVC = segue.destinationViewController as! secondViewController
+        
+        newVC.tmpCount = myCount
+    }
+
+    
 
 
 }
